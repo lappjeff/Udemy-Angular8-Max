128e3dd530086e2a7f15a6a005690ddf99086bec
Jeff Lapp <lappjeff20@gmail.com>
2020-01-24 16:29:57 -0500

    Set up examples of attribute and class angular selectors

--- app-1/my-first-app/src/app/servers/servers.component.ts
+++ app-1/my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-servers",
+  // select by html selector
+  selector: "app-servers"
+  // select by attribute
+  // selector: "[app-servers]",
+  // select by classname
+  // selector: ".app-servers",
   template: `
     <app-server></app-server> <app-server></app-server>
   `,
