08d6c986f0f7e476fa0ef2397206376683736f24
Jeff Lapp <lappjeff20@gmail.com>
2020-01-24 16:25:03 -0500

    Messing around with styling options

--- app-1/my-first-app/src/app/app.component.ts
+++ app-1/my-first-app/src/app/app.component.ts
@@ -3,6 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  // styleUrls: ["./app.component.css"]
+  // can also use inline styles
+  styles: [
+    `
+      h3 {
+        color: dodgerblue;
+      }
+    `
+  ]
 })
 export class AppComponent {}
