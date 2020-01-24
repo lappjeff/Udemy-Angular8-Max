2aeca6ecd49cfb15f65447ce5496a49b71448573
Jeff Lapp <lappjeff20@gmail.com>
2020-01-24 17:00:24 -0500

    Removed all refs to test components

--- app-1/my-first-app/src/app/app.module.ts
+++ app-1/my-first-app/src/app/app.module.ts
@@ -3,17 +3,9 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from "./servers/servers.component";
-import { SuccessAlertComponent } from "./success-alert/success-alert.component";
-import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
-  ],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
--- app-1/my-first-app/src/app/warning-alert/warning-alert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-warning-alert",
-  template: `
-    <h2 class="bg-warning">Something went wrong!</h2>
-  `
-})
-export class WarningAlertComponent {}
