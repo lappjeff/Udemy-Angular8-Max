17a88ece70758fedca10203c642c234bf5e1aab2
Jeff Lapp <lappjeff20@gmail.com>
2020-01-27 17:23:47 -0500

    Replaced basic event binding with ngModel binding

--- app-1/my-first-app/src/app/app.module.ts
+++ app-1/my-first-app/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from "./servers/servers.component";
-
+import { FormsModule } from "@angular/forms";
 @NgModule({
   declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
