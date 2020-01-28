4ba1c9b9c97750067c82eba6d8808c64e9412778
Jeff Lapp <lappjeff20@gmail.com>
2020-01-28 17:14:53 -0500

    Generated recipe detail component, added all new components to app.module.ts

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RecipeListComponent } from "./recipe-book/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "./recipe-book/recipe-item/recipe-item.component";
+import { RecipeDetailComponent } from "./recipe-book/recipe-detail/recipe-detail.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
