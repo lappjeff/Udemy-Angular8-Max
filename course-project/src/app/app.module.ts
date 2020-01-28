435d55aefdb39f86d595711d7d263b98f0a5ef62
Jeff Lapp <lappjeff20@gmail.com>
2020-01-28 17:23:57 -0500

    Generated header component

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RecipeItemComponent } from "./recipe-book/recipe-item/recipe-item.compo
 import { RecipeDetailComponent } from "./recipe-book/recipe-detail/recipe-detail.component";
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/sh
     RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent
+    ShoppingListEditComponent,
+    HeaderComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
