9a930eec249be151a1f5406456204d0feeefc0ab
Jeff Lapp <lappjeff20@gmail.com>
2020-01-29 17:02:38 -0500

    Fixed example bug

--- debugging/debugging/src/app/app.component.ts
+++ debugging/debugging/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  servers;
+  servers: string[] = [];
 
   onAddServer() {
-    this.servers.push('Another Server');
+    this.servers.push("Another Server");
   }
 
   onRemoveServer(id: number) {
