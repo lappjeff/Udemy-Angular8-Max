1ababd91e51bb358036211cd145b07048fc5fafb
Jeff Lapp <lappjeff20@gmail.com>
2020-01-30 11:40:50 -0500

    Set up onStartGame function

--- cmp-databinding-assignment-start/src/app/gamecontrol/gamecontrol.component.ts
+++ cmp-databinding-assignment-start/src/app/gamecontrol/gamecontrol.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-gamecontrol',
-  templateUrl: './gamecontrol.component.html',
-  styleUrls: ['./gamecontrol.component.css']
+  selector: "app-gamecontrol",
+  templateUrl: "./gamecontrol.component.html",
+  styleUrls: ["./gamecontrol.component.css"]
 })
 export class GamecontrolComponent implements OnInit {
+  interval;
+  intervalFired = new EventEmitter<number>();
+  lastNumber = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber += 1;
+    }, 1000);
   }
-
 }
