5e2b9fb217fffefe226bec8e60b21b5d0fa01316
Jeff Lapp <lappjeff20@gmail.com>
2020-02-04 16:57:07 -0500

    Injected accounts-service into app-component

--- services-start/src/app/app.component.ts
+++ services-start/src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AccountsService } from "./accounts-service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  providers: [AccountsService]
 })
-export class AppComponent {
-  onAccountAdded(newAccount: { name: string; status: string }) {
-    this.accounts.push(newAccount);
-  }
+export class AppComponent implements OnInit {
+  accounts: { name: string; status: string }[] = [];
 
-  onStatusChanged(updateInfo: { id: number; newStatus: string }) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  constructor(private accountsService: AccountsService) {}
+
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts;
   }
 }
