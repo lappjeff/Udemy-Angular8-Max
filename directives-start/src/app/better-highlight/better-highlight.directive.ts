16bb571b60566df3e9a0cbaedcbed038b8ed6827
Jeff Lapp <lappjeff20@gmail.com>
2020-02-04 15:02:50 -0500

    Completed better-highlight using Render vs direct DOM manipulation

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef } from "@angular/core";
 
 @Directive({
-  selector: '[appBetterHighlight]'
+  selector: "[appBetterHighlight]"
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
-  constructor() { }
-
+  ngOnInit() {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      "background-color",
+      "blue"
+    );
+  }
 }
