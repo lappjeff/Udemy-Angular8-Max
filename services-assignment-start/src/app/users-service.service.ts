3ebfd6e0cb4f4084260f7d6d66b1513751068073
Jeff Lapp <lappjeff20@gmail.com>
2020-02-06 11:48:08 -0500

    Added functions for adding users to inactive and active arrays

--- services-assignment-start/src/app/users-service.service.ts
+++ services-assignment-start/src/app/users-service.service.ts
@@ -8,4 +8,14 @@ export class UsersService {
   inactiveUsers: string[] = ["Chris", "Manu"];
 
   constructor() {}
+
+  setInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+
+  setActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
 }
