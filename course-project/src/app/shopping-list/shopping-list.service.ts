b423f75fa71c6c12c5f6d4eb1e1688ec493ec663
Jeff Lapp <lappjeff20@gmail.com>
2020-02-06 13:32:34 -0500

    Modified @addIngredient function to accept and add an array of ingredients

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -17,8 +17,10 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
-  addIngredient(ingredient: Ingredient): void {
-    this.ingredients.push(ingredient);
+  addIngredient(ingredients: Ingredient[]): void {
+    ingredients.forEach((ingredient: Ingredient) =>
+      this.ingredients.push(ingredient)
+    );
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
