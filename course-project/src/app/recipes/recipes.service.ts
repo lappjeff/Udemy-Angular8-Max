abc9598c5f05b057e0a544d1ecdeab49ce19f64c
Jeff Lapp <lappjeff20@gmail.com>
2020-02-06 12:50:13 -0500

    Started work on RecipesService, added recipes array

--- course-project/src/app/recipes/recipes.service.ts
+++ course-project/src/app/recipes/recipes.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RecipesService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "Test recipe",
+      "https://natashaskitchen.com/wp-content/uploads/2018/08/Chicken-Stir-Fry-1-1.jpg"
+    ),
+    new Recipe(
+      "Another test recipe",
+      "Test recipe",
+      "https://natashaskitchen.com/wp-content/uploads/2018/08/Chicken-Stir-Fry-1-1.jpg"
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
