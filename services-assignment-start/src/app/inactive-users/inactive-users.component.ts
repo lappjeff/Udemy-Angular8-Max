fd3fd6249e7a75616617d491f2653e97ef240014
Jeff Lapp <lappjeff20@gmail.com>
2020-02-06 11:42:54 -0500

    Replaced @Input inactive users with users from UsersService

--- services-assignment-start/src/app/inactive-users/inactive-users.component.ts
+++ services-assignment-start/src/app/inactive-users/inactive-users.component.ts
@@ -1,14 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
+import { UsersService } from "../users-service.service";
 
 @Component({
-  selector: 'app-inactive-users',
-  templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  selector: "app-inactive-users",
+  templateUrl: "./inactive-users.component.html",
+  styleUrls: ["./inactive-users.component.css"]
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
+export class InactiveUsersComponent implements OnInit {
+  users: string[];
   @Output() userSetToActive = new EventEmitter<number>();
 
+  constructor(private usersService: UsersService) {}
+  ngOnInit() {
+    this.users = this.usersService.inactiveUsers;
+  }
   onSetToActive(id: number) {
     this.userSetToActive.emit(id);
   }
