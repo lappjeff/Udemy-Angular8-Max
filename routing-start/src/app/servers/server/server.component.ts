32e637cc1ad0f92482b36cf9503cdfeeedfb7859
Jeff Lapp <lappjeff20@gmail.com>
2020-02-10 13:57:28 -0500

    Removed server rendering in servers.component.html

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 import { ServersService } from "../servers.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: "app-server",
@@ -19,5 +19,8 @@ export class ServerComponent implements OnInit {
   ngOnInit() {
     const id = parseInt(this.route.snapshot.params.id);
     this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(id);
+    });
   }
 }
