0c6307f7e11c80a7bb22ede6cad881bb31ce5509
Jeff Lapp <lappjeff20@gmail.com>
2020-02-10 13:58:37 -0500

    Fixed type of arg passed to params observable

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -20,7 +20,7 @@ export class ServerComponent implements OnInit {
     const id = parseInt(this.route.snapshot.params.id);
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
-      this.server = this.serversService.getServer(id);
+      this.server = this.serversService.getServer(parseInt(params.id));
     });
   }
 }
