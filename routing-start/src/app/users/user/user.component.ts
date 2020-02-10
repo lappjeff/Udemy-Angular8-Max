0163fda8ff992b1ebe992a2fdfd7d7299c8958fa
Jeff Lapp <lappjeff20@gmail.com>
2020-02-10 13:23:13 -0500

    Added unsubscribe call for params observable

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"]
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
-
+  paramsSub: Subscription;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
@@ -16,9 +17,14 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params["id"],
       name: this.route.snapshot.params["name"]
     };
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSub = this.route.params.subscribe((params: Params) => {
       this.user.id = params["id"];
       this.user.name = params["name"];
     });
+  }
+
+  ngOnDestroy() {
+    // Angular does this for us already, but it's a good reminder that sometimes observables need manually destroyed/unsubscribed
+    this.paramsSub.unsubscribe();
   }
 }
