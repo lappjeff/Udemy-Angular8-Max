b04cf22094d7508266a285bc2f6ef31253b82e68
Jeff Lapp <lappjeff20@gmail.com>
2020-02-10 16:20:07 -0500

    Set up route guards with Angular route protection

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
+import { AuthGuardService } from "./auth-guard.service";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -17,6 +18,7 @@ const appRoutes: Routes = [
   },
   {
     path: "servers",
+    canActivate: [AuthGuardService],
     component: ServersComponent,
     children: [
       { path: ":id", component: ServerComponent },
--- routing-start/src/app/auth-guard.service.ts
+++ routing-start/src/app/auth-guard.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService
+      .isAuthenticated()
+      .then((isAuthenticated: boolean) => {
+        if (isAuthenticated) {
+          return true;
+        } else {
+          this.router.navigate(["/"]);
+        }
+      });
+  }
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 }
