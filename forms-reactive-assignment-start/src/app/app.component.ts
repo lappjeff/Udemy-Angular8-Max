b5c3307808c536a69f1cea64a0da665a08dbb334
Jeff Lapp <lappjeff20@gmail.com>
2020-02-24 17:53:39 -0500

    Set up form shape object and html

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  statusOptions = ["Stable", "Critical", "Finished"];
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      status: new FormControl("Stable")
+    });
+  }
 }
