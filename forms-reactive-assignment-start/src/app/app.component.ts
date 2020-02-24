55b93a0c345f7282f9233153fa51a8d100e75d64
Jeff Lapp <lappjeff20@gmail.com>
2020-02-24 18:34:18 -0500

    Finished reactive forms assignment

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "app-root",
@@ -12,9 +13,37 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, Validators.required),
+      projectName: new FormControl(
+        null,
+        [Validators.required],
+        this.forbiddenName
+      ),
       email: new FormControl(null, [Validators.required, Validators.email]),
       status: new FormControl("Stable")
     });
+  }
+
+  // forbiddenName(control: FormControl): { [s: string]: boolean } {
+  //   if (control.value === "Test") {
+  //     return { nameIsForbidden: true };
+  //   }
+  //   return null;
+  // }
+
+  forbiddenName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((res, rej) => {
+      setTimeout(() => {
+        if (control.value === "Test") {
+          res({ nameIsForbidden: true });
+        } else {
+          res(null);
+        }
+      }, 3000);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
   }
 }
