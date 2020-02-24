3cb5ca293c00d793b72a8b4b4fccd80f29759c99
Jeff Lapp <lappjeff20@gmail.com>
2020-02-24 13:45:23 -0500

    Started building out email, password and subscription inputs

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-
+  @ViewChild("f", { static: true }) signupForm: NgForm;
+  defaultSubscription = "Advanced";
 }
