c6e56632e5ef998b2e250e045e2bafaf133625a8
Jeff Lapp <lappjeff20@gmail.com>
2020-02-24 13:25:28 -0500

    Added assignment files

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -41,5 +41,7 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
