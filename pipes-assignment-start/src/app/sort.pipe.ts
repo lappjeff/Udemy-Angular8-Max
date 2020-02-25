904090833db016addeca059774fe19f9e39f523e
Jeff Lapp <lappjeff20@gmail.com>
2020-02-25 15:46:46 -0500

    Completed sort function

--- pipes-assignment-start/src/app/sort.pipe.ts
+++ pipes-assignment-start/src/app/sort.pipe.ts
@@ -1,12 +1,20 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'sort'
+  name: "sort"
 })
 export class SortPipe implements PipeTransform {
-
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any, sortProperty: string): any {
+    if (value.length === 0) {
+      return value;
+    } else {
+      return value.sort((a, b) => {
+        if (a[sortProperty] > b[sortProperty]) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    }
   }
-
 }
