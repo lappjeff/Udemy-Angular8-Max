3cbd2a9d9650665d610a182513e363231f8d91c3
Jeff Lapp <lappjeff20@gmail.com>
2020-02-25 12:42:45 -0500

    Added add ingredient functionality

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
-import { FormControl, FormGroup } from "@angular/forms";
+import { Ingredient } from "./../../shared/ingredient.model";
+import { FormControl, FormGroup, FormArray } from "@angular/forms";
 import { ActivatedRoute, Params } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
 import { RecipesService } from "../recipes.service";
@@ -12,6 +13,8 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+  recipeIngredients = new FormArray([]);
+
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipesService
@@ -29,6 +32,18 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get("ingredients")).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    );
+  }
+  get controls() {
+    // a getter!
+    return (<FormArray>this.recipeForm.get("ingredients")).controls;
+  }
   private initForm() {
     let recipeName = "";
     let recipeImagePath = "";
@@ -39,11 +54,22 @@ export class RecipeEditComponent implements OnInit {
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe["ingredients"]) {
+        for (let ingredient of recipe.ingredients) {
+          this.recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription)
+      description: new FormControl(recipeDescription),
+      ingredients: this.recipeIngredients
     });
   }
 }
