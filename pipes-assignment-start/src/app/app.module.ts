a84aa98b2514034c49bb970fd1d8926b2fb2e92f
Jeff Lapp <lappjeff20@gmail.com>
2020-02-25 15:36:50 -0500

    Generated reverse and sort pipe templates

--- pipes-assignment-start/src/app/app.module.ts
+++ pipes-assignment-start/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortPipe
   ],
   imports: [
     BrowserModule,
