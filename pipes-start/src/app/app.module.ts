124ce2527041631dde38f3b4a21dd02683e165ac
Jeff Lapp <lappjeff20@gmail.com>
2020-02-25 13:54:02 -0500

    Set up filter pipe

--- pipes-start/src/app/app.module.ts
+++ pipes-start/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
