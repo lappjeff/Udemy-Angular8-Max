3a4c36ceb2fde025612cd0c9bede0f0d7aff8368
Jeff Lapp <lappjeff20@gmail.com>
2020-02-25 12:25:25 -0500

    Started building out recipe edit Reactive FOrm

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
+import { FormControl, FormGroup } from "@angular/forms";
 import { ActivatedRoute, Params } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
+import { RecipesService } from "../recipes.service";
 
 @Component({
   selector: "app-recipe-edit",
@@ -9,13 +11,35 @@ import { Component, OnInit } from "@angular/core";
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
-
-  constructor(private route: ActivatedRoute) {}
+  recipeForm: FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipesService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = parseInt(params["id"]);
       this.editMode = params["id"] != null;
+      this.initForm();
+    });
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let recipeDescription = "";
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription)
     });
   }
 }
