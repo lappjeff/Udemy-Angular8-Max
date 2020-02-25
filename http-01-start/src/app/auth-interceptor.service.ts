1d76fdc33053a229fbbd66347f2b2a2dfad36536
Jeff Lapp <lappjeff20@gmail.com>
2020-02-25 17:27:40 -0500

    Built http interceptor

---
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -0,0 +1,12 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler
+} from "@angular/common/http";
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log("request is on its way, intercepted");
+    return next.handle(req);
+  }
+}
