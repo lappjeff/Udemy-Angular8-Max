fe8823db5677f9c959a00a0794366c8ccda5280f
Jeff Lapp <lappjeff20@gmail.com>
2020-02-25 16:10:30 -0500

    Set up post request using http library

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,28 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
-
+  baseUrl = "https://ng-complete-guide-39db7.firebaseio.com/";
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    this.http.post(`${this.baseUrl}/posts.json`, postData).subscribe(data => {
+      console.log(data);
+    });
   }
 
   onFetchPosts() {
