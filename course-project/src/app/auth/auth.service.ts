5b9e4e648297fd3738c55386c77503489b2dfa50
Jeff Lapp <lappjeff20@gmail.com>
2020-02-26 12:44:15 -0500

    Built sign up function

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
+  signUpUrl =
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBEFQPL26N7h1tTqLVosA8KTsKFaDqOXkM";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(this.signUpUrl, {
+      email,
+      password,
+      returnSecureToken: true
+    });
+  }
 }
