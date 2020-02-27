bfe7cc756cbbad11967a9683ad55d9757e9a18c2
Jeff Lapp <lappjeff20@gmail.com>
2020-02-27 13:43:55 -0500

    Implemented lazy loading for all main routes

--- course-project/src/app/auth/auth-routing.module.ts
+++ course-project/src/app/auth/auth-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
 
-const authRoutes: Routes = [{ path: "auth", component: AuthComponent }];
+const authRoutes: Routes = [{ path: "", component: AuthComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(authRoutes)],
