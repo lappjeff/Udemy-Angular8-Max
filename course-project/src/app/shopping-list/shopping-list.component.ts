02825c63376512204db8bed40c349e7538a9892c
Jeff Lapp <lappjeff20@gmail.com>
2020-03-02 11:51:33 -0500

    Started setting up reducer for shopping list

--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -2,26 +2,24 @@ import { Component, OnInit } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
 import { LoggingService } from "../logging.service";
-
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 @Component({
   selector: "app-shopping-list",
   templateUrl: "./shopping-list.component.html",
   styleUrls: ["./shopping-list.component.scss"]
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
   constructor(
     private shoppingList: ShoppingListService,
-    private logService: LoggingService
+    private logService: LoggingService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
   ) {}
 
   ngOnInit() {
-    this.ingredients = this.shoppingList.getIngredients();
-    this.shoppingList.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.ingredients = this.store.select("shoppingList");
+
     this.logService.printLog("Hello from shopping list component ngOnit");
   }
 
